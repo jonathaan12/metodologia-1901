e0919fbc343c04d9ebfdb83e566dc93a7862cd23
T-107 <T-107@PC194>
2018-09-17 17:41:20 -0500

    Segunda Clase

---
+++ Metodologia-1901/src/metodologia/pkg1901/Metodologia1901.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodologia.pkg1901;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author T-107
+ */
+public class Metodologia1901 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola mundo");
+        JOptionPane.showConfirmDialog(null, "Hola Mundo");
+        // TODO code application logic here
+    }
+    
+}
